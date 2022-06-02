bd3bcf93c151636385829353225065cc56897135
KSMubasshir <ksmubasshir@gmail.com>
2022-06-02 01:56:02 -0400

    media and static url fixed at namecheap server

--- hashimukhDjangoProject/settings.py
+++ hashimukhDjangoProject/settings.py
@@ -130,19 +130,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/statics')
+STATIC_URL = '/statics/'
 STATIC_ROOT = '/home/hashzbix/hashimukh/hashimukh/statics/'
+# STATIC_ROOT = os.path.join(BASE_DIR, '/statics')
+# STATIC_ROOT = '/home/hashzbix/hashimukh/hashimukh/statics/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'statics'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'statics'),
+]
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/hashzbix/hashimukh/media/'
 
 # Path where media is stored
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
