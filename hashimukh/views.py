eb60ae3b2b7b2d5b7ceae268c2f53eed0a07d67f
KSMubasshir <ksmubasshir@gmail.com>
2022-06-09 16:56:41 +0600

    value loaded in dropdowns

--- hashimukh/views.py
+++ hashimukh/views.py
@@ -18,17 +18,34 @@ def index(request):
 
 def contact(request):
     template = loader.get_template('contacts/contact.html')
-    context = {}
+    all_news_list = News.objects.all()
+    all_project_list = Project.objects.all()
+    context = {
+        'all_news_list': all_news_list,
+        'all_project_list': all_project_list
+    }
     return HttpResponse(template.render(context, request))
 
 
 def news(request, news_id):
     news = News.objects.get(news_id=news_id)
-    context = {'news': news}
+    all_news_list = News.objects.all()
+    all_project_list = Project.objects.all()
+    context = {
+        'news': news,
+        'all_news_list': all_news_list,
+        'all_project_list': all_project_list
+    }
     return render(request, 'news/news.html', context)
 
 
 def project(request, project_id):
     project = Project.objects.get(project_id=project_id)
-    context = {'project': project}
+    all_news_list = News.objects.all()
+    all_project_list = Project.objects.all()
+    context = {
+        'project': project,
+        'all_news_list': all_news_list,
+        'all_project_list': all_project_list
+    }
     return render(request, 'projects/project.html', context)
