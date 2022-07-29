50687e16de1a7457b4c9f5711f720712a0601a08
KSMubasshir <ksmubasshir@gmail.com>
2022-07-29 23:20:05 +0600

    layout updated

--- hashimukh/views.py
+++ hashimukh/views.py
@@ -22,9 +22,11 @@ def contact(request):
     template = loader.get_template('contacts/contact.html')
     all_news_list = News.objects.all()
     all_project_list = Project.objects.all()
+    all_focus_list = Focus.objects.all()
     context = {
         'all_news_list': all_news_list,
-        'all_project_list': all_project_list
+        'all_project_list': all_project_list,
+        'all_focus_list': all_focus_list
     }
     return HttpResponse(template.render(context, request))
 
@@ -33,10 +35,12 @@ def news(request, news_id):
     news = News.objects.get(news_id=news_id)
     all_news_list = News.objects.all()
     all_project_list = Project.objects.all()
+    all_focus_list = Focus.objects.all()
     context = {
         'news': news,
         'all_news_list': all_news_list,
-        'all_project_list': all_project_list
+        'all_project_list': all_project_list,
+        'all_focus_list': all_focus_list
     }
     return render(request, 'news/news.html', context)
 
@@ -45,10 +49,12 @@ def project(request, project_id):
     project = Project.objects.get(project_id=project_id)
     all_news_list = News.objects.all()
     all_project_list = Project.objects.all()
+    all_focus_list = Focus.objects.all()
     context = {
         'project': project,
         'all_news_list': all_news_list,
-        'all_project_list': all_project_list
+        'all_project_list': all_project_list,
+        'all_focus_list': all_focus_list
     }
     return render(request, 'projects/project.html', context)
 
@@ -57,9 +63,11 @@ def donate(request):
     template = loader.get_template('donation/donate.html')
     all_news_list = News.objects.all()
     all_project_list = Project.objects.all()
+    all_focus_list = Focus.objects.all()
     context = {
         'all_news_list': all_news_list,
-        'all_project_list': all_project_list
+        'all_project_list': all_project_list,
+        'all_focus_list': all_focus_list
     }
     return HttpResponse(template.render(context, request))
 
@@ -68,9 +76,11 @@ def joinus(request):
     template = loader.get_template('joinus/joinus.html')
     all_news_list = News.objects.all()
     all_project_list = Project.objects.all()
+    all_focus_list = Focus.objects.all()
     context = {
         'all_news_list': all_news_list,
-        'all_project_list': all_project_list
+        'all_project_list': all_project_list,
+        'all_focus_list': all_focus_list
     }
     return HttpResponse(template.render(context, request))
 
@@ -80,10 +90,12 @@ def focus(request, focus_id):
     all_news_list = News.objects.all()
     all_project_list = Project.objects.all()
     focussed_projects = FocussedProject.objects.filter(focus_id=focus_id)
+    all_focus_list = Focus.objects.all()
     context = {
         'focus': focus,
         'all_news_list': all_news_list,
         'all_project_list': all_project_list,
-        'focussed_projects': focussed_projects
+        'focussed_projects': focussed_projects,
+        'all_focus_list': all_focus_list
     }
     return render(request, 'focus/focus.html', context)
